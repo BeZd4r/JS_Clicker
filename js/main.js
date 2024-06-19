f96ad3be273141bb5557065ada24a6342935a163
BeZd4r <103284584+BeZd4r@users.noreply.github.com>
2024-06-20 01:51:37 +0300

    Add an animal bar + revork EventListener

--- js/main.js
+++ js/main.js
@@ -1,38 +1,74 @@
 const counter = document.getElementById("Counter");
-const homo = document.getElementById("Homo"); 
+const animal = document.getElementById("Animal"); 
+const animalBar = document.getElementById("choose-animal-bar")
 
-const homoStartWidth = 400;
-const homoStartHeight = 400;
-const homoAnimationHeight = 100;
-const homoAnimationWidth = 100;
-
+const animalStartWidth = 350;
+const animalStartHeight = 350;
+const animalAnimationHeight = 100;
+const animalAnimationWidth = 100;
 
 let clicks = 0;
 
-homo.width = homoStartWidth;
-homo.height = homoStartHeight;
+function drawMainPage(){
 
-function onClickHomo(){
-    clicks++;
-    counter.textContent = `Вы сделали ${clicks} клик(ов)`;
-    HomoAnimation(clicks);
+    animal.width = animalStartWidth;
+    animal.height = animalStartHeight;
+
+    let animalBarEvents = new AnimalBarEvents();
+    animal.addEventListener("click",clickAnimal);
+    for (let i = 1; i < 6;i++){
+        let animals = document.createElement("img");
+        animals.src = `source/animals/animal_${i}.png`;
+        animals.id = `animal_${i}`;
+        animals.height = 80;
+        animals.width = 80;
+        animals.addEventListener("click",animalBarEvents);
+        animalBar.appendChild(animals);
+    }
 }
 
-function HomoAnimation(currentclicks){
-    homo.width = homoAnimationWidth;
-    homo.height = homoAnimationHeight;
+class AnimalBarEvents{
+    handleEvent(event) {
+        switch(event.type) {
+            case 'click':
+                let target = event.currentTarget;
+                target.width = 50;
+                target.height = 50;
+                
+                let timer = setTimeout(function increase() { 
+                    console.log("event.currentTarget.width");
+                    target.width = 80;
+                    target.height = 80;
+                },50)
+
+                animal.src = `source/animals/${target.id}.png`;
+                break;
+        }
+    }
+}
+
+function clickAnimal(){
+    clicks++;
+    counter.textContent = `Вы сделали ${clicks} клик(ов)`;
+    animalAnimation(clicks);
+}
+
+function animalAnimation(currentclicks){
+    animal.width = animalAnimationWidth;
+    animal.height = animalAnimationHeight;
     
-    let timer = setInterval(resizeHomo,1);
+    let timer = setInterval(resizeAnimal,1);
 
-    function resizeHomo(){
+    function resizeAnimal(){
 
-        homo.height += 10;
-        homo.width += 10;
+        animal.height += 10;
+        animal.width += 10;
 
-        if (homo.width == homoStartWidth || 
-        homo.height == homoStartHeight || currentclicks != clicks ){
+        if (animal.width == animalStartWidth || 
+        animal.height == animalStartHeight || currentclicks != clicks ){
             clearTimeout(timer)
         }
     }
-  
 }
+
+drawMainPage();
